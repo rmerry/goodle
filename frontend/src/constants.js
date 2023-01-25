2c36d40777ccf4a96aa4bc5ef07a664b15cdf394
Matthew Elliston <mpgelliston@gmail.com>
2023-01-25 22:54:46 +0000

    Added tweak to the initial template. Added production URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,5 @@
-export const API_URL = "http://localhost:4001";
+// TODO Inject .env variables or make it so that this isnt hardcoded
+export const API_URL =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://localhost:4001"
+    : "http://178.62.73.7:4001/";
