c5e18e303c7db38189596094d459d82450791c0a
Matthew Elliston <mpgelliston@gmail.com>
2023-01-31 15:10:51 +0000

    Changes to the front end to move the signup into the creation of an event

--- frontend/src/routes/event.jsx
+++ frontend/src/routes/event.jsx
@@ -4,7 +4,7 @@ import {getEvent, addAttendee, removeAttendee} from '../actions/event';
 import Signup from "../components/signup";
 import { loadUser } from "./root";
 
-// TODO Could add the event hash of the user to the localstorage so that it is semi-persistent
+// TODO Could add the event hash of the user to the localStorage so that it is semi-persistent
 
   const days = [
     "Sun",
--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Form, redirect } from "react-router-dom";
 import { createEvent } from "../actions/event";
-import Signup from "../components/signup";
+import { saveUser } from "../actions/user";
 import { validateEmail } from "../helper/email";
 
 const defaultUser = {
@@ -17,8 +17,20 @@ export const loadUser = () => {
 
 export async function  createEventAction({ request, params }) {
   const formData = await request.formData();
-  const event = Object.fromEntries(formData);
+  const data = Object.fromEntries(formData);
+
+  // See if there is a user to create
+  if (data.hasOwnProperty('name') && data.hasOwnProperty('email')){
+    const {name, email} = data;
+    const user = { name, email, accountCreated: true };
+    saveUser(user);
+    delete data['name'];
+    delete data['email'];
+  }
+  // TODO Refactor this out perhaps to two actions at a later date.
+  const event = {...data};
   const response = await createEvent(event);
+
   if (response !== undefined) {
     return redirect(`/event/${response.hash}`);
   } else {
@@ -27,57 +39,20 @@ export async function  createEventAction({ request, params }) {
   }
 }
 
-const renderCreateEvent = (title, description, setTitle, setDescription) => {
-  const emptyTitle = title.length > 0 ? false : true;
-  return (
-    <section id="createEvent">
-      <h2>Create an Event</h2>
-      <Form method="post">
-        <label className="block">
-          <span className="text-gray-700">Name:</span>
-          <input
-            type="text"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Name of the Event"
-            aria-label="email"
-            aria-invalid={emptyTitle ? null : title.length > 0 ? false : true}
-            className="form-input"
-          />
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Description:</span>
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Event Description"
-            aria-label="email"
-            className="form-input"
-            name="description"
-          />
-        </label>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          type="submit"
-        >
-          Create Event
-        </button>
-      </Form>
-    </section>
-  );
-}
-
-
 function App() {
   // Load any stored user
   const user = loadUser();
 
-  const [accountCreated, setAccountCreated] = useState(user.accountCreated);
+  const [userName, setUserName] = useState(user.name);
+  const [userEmail, setUserEmail] = useState(user.email);
+  const [accountCreated, ] = useState(user.accountCreated);
   
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   
+  const emptyTitle = title.length > 0 ? false : true;
+  const emptyName = userName.length > 0 ? false : true;
+  const emptyEmail = userEmail.length > 0 ? false : true;
   return (
     <>
       <header className="container">
@@ -85,9 +60,87 @@ function App() {
           <h1>Goodle!</h1>
           <p>Welcome {user.name}</p>
         </hgroup>
-      </header>  
+      </header>
       <main className="container">
-      {!accountCreated ? <Signup name="" email="" callback={() => setAccountCreated(true)}/> : renderCreateEvent(title, description, setTitle, setDescription)}
+        <section id="createEvent">
+          <h2>Create an Event</h2>
+          <Form method="post">
+            {!accountCreated ? (
+              <>
+                <label className="block">
+                  <span className="text-gray-700">Name:</span>
+                  <input
+                    type="text"
+                    name="name"
+                    value={userName}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                    placeholder="Your name, nickname whatever"
+                    aria-label="name"
+                    aria-invalid={
+                      emptyName ? null : userName.length > 0 ? false : true
+                    }
+                    className="form-input"
+                  />
+                </label>
+                <label className="block">
+                  <span className="text-gray-700">Email:</span>
+                  <input
+                    type="email"
+                    name="email"
+                    value={userEmail}
+                    onChange={(e) => {
+                      setUserEmail(e.target.value);
+                    }}
+                    placeholder="Email, so we can email even updates or whatever"
+                    aria-label="email"
+                    aria-invalid={
+                      emptyEmail
+                        ? null
+                        : validateEmail(userEmail)
+                        ? false
+                        : true
+                    }
+                    className="form-input"
+                  />
+                </label>
+              </>
+            ) : null}
+            <label className="block">
+              <span className="text-gray-700">Name:</span>
+              <input
+                type="text"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Name of the Event"
+                aria-label="email"
+                aria-invalid={
+                  emptyTitle ? null : title.length > 0 ? false : true
+                }
+                className="form-input"
+              />
+            </label>
+            <label className="block">
+              <span className="text-gray-700">Description:</span>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Event Description"
+                aria-label="email"
+                className="form-input"
+                name="description"
+              />
+            </label>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Create Event
+            </button>
+          </Form>
+        </section>
       </main>
     </>
   );
