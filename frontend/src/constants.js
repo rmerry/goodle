2bec0f74ed983a57518ae901072fb6e7c838d9bd
Matthew Elliston <mpgelliston@gmail.com>
2023-02-02 22:52:46 +0000

    Added the production url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,4 +2,4 @@
 export const API_URL =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
     ? "http://localhost:4001"
-    : "http://178.62.73.7:4001/";
+    : "https://api-goodle.codestar.com";
